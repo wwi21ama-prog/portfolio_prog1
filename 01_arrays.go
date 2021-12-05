7e6548014d915ce24d358c7c1bc45283a614a400
ReinerHuechting <46283923+ReinerHuechting@users.noreply.github.com>
2021-12-05 13:04:43 +0100

    Kommentar verkürzt.

--- 01_arrays.go
+++ 01_arrays.go
@@ -1,9 +1,9 @@
 package main
 
-/*** AUFGABENSTELLUNG:
-    Schreiben Sie eine Funktion multfive(), die eine Liste von Zahlen erwartet.
-    Die Funktion soll das Produkt der Elemente an den durch 5 teilbaren Positionen zurückliefern.
-***/
+/* AUFGABENSTELLUNG:
+Schreiben Sie eine Funktion multfive(), die eine Liste von Zahlen erwartet.
+Die Funktion soll das Produkt der Elemente an den durch 5 teilbaren Positionen zurückliefern.
+*/
 func multfive(liste []int) int {
 	// TODO
 	return 0
