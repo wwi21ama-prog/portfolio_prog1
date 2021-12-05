6b9047a2225148e122bde9624e3733a7617cdc1c
ReinerHuechting <46283923+ReinerHuechting@users.noreply.github.com>
2021-12-05 14:15:22 +0100

    Aufgabe 7 hinzugefügt.

---
+++ 07_game.go
@@ -0,0 +1,26 @@
+package main
+
+/* AUFGABENSTELLUNG:
+Nutzen Sie die Spielfeld-Datenstrukturen aus der Vorlesung,
+um das Spiel "2048" zu implementieren (vgl. https://play2048.co).
+
+Mögliche Vorgehensweise: Definieren Sie eine Funktion, die das
+Spielfeld sowie eine Richtungsangabe erwartet und die daraus
+das Folge-Spielfeld berechnet.
+*/
+
+func run_2048() {
+	// TODO
+}
+
+/* HINWEISE:
+Dies ist eine fortgeschrittene Aufgabe.
+Definieren Sie sich geeignete Hilfsfunktionen und -Datenstrukturen,
+Erläutern Sie Ihren Lösungsansatz kurz mittels Kommentaren.
+
+Bei dieser Aufgabe werden Korrektheit und Modularität/Flexibilität
+der Imnplementierung bewertet.
+Außerdem wird bewertet, wie "geschliffen" das Spiel am Ende ist,
+d.h. inwieweit Sie Besonderheiten, Gewinnbedingungen etc.
+berücksichtigt haben, .
+*/
