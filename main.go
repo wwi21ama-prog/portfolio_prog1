06805b56ac47b0cdaed0a5f50b8888af2bf4b371
ReinerHuechting <46283923+ReinerHuechting@users.noreply.github.com>
2021-12-05 13:00:11 +0100

    Erklärung in main() hinzugefügt.

--- main.go
+++ main.go
@@ -1,4 +1,8 @@
 package main
 
 func main() {
+	// Die main()-Funktion ist leer, weil die Tests in separaten Dateien formuliert sind
+	// und mit "go test" auf der Konsole gestartet werden können.
+	// Sie brauchen für die Aufgaben also nicht unbedingt eine main()-Funktion.
+	// Sie können diese Funktion aber bei der Entwicklung/Fehlersuche verwenden.
 }
